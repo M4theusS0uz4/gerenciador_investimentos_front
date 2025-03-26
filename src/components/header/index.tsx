a7f07d37d38cad909f97801f4ba6aa7419845ea6
M4theusS0uz4 <matheusrocham.ms@gmail.com>
2025-03-25 22:13:25 -0300

    Using Logo and Making Navbar

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 
@@ -7,24 +7,23 @@ export default function Header() {
   const [isAuthenticated, setIsAuthenticated] = useState(false); // Simulando login
 
   return (
-    <header className="bg-gray-900 text-white p-4 flex justify-between items-center">
-      <h1 className="text-xl font-bold">Logo</h1>
-      
+    <header className="bg-white text-white p-4 flex justify-between items-center">
+      <Image src="/logoPI.png" alt="Logo" width={200} height={100} className="w-32 h-auto"/>
       {!isAuthenticated ? (
         <div className="space-x-4">
           <Link href="/login">
-            <button className="px-4 py-2 bg-white-600 rounded-md hover:bg-blue-500">
+            <button className="px-4 py-2 bg-white text-black rounded-md hover:bg-black hover:text-white">
               Login
             </button>
           </Link>
           <Link href="/criar-conta">
-            <button className="px-4 py-2 bg-black-600 text-color= white rounded-md hover:bg-green-500">
+            <button className="px-4 py-2 bg-black text-white rounded-md hover:bg-green-500">
               Criar Conta
             </button>
           </Link>
         </div>
       ) : (
-        <p>Bem-vindo!</p>
+        <p className="text-black">Bem-vindo!</p>
       )}
     </header>
   );
